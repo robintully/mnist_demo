eaad9c04d2dff85707dfd437a07ea096b0609825
Robin Tully <mr.robintully@gmail.com>
2017-09-14 00:08:24 -0400

    direct import for keras_workers

--- app/keras_worker.py
+++ app/keras_worker.py
@@ -3,8 +3,8 @@ from keras.models import load_model
 
 
 # Generally want this to load from s3, or pull in externally rather then expect file directly
-model = load_model('trained_mnist.h5')
-dummy_questions = h5py.File('dummy_questions.h5', 'r')['dataset_1'][:]
+model = load_model('app/trained_mnist.h5')
+dummy_questions = h5py.File('app/dummy_questions.h5', 'r')['dataset_1'][:]
 
 
 def mnist_by_index(num):
